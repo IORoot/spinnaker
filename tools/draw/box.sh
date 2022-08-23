b1ae1267d9b6d3cd4b0e6539d61a451c3dd3193a
Andy Pearson <andy@londonparkour.com>
2022-08-23 13:27:03 +0100

    single emoji working with padding

--- tools/draw/box.sh
+++ tools/draw/box.sh
@@ -7,6 +7,68 @@ repeat_character(){
     for (( i = 0; i < $1; ++i )); do echo -n "$2"; done
 }
 
+
+# For a multi-line string (with possible emoji)
+# Calculate the length of the longest line.
+longest_line_width(){
+
+    TEXT_STRING=$1
+
+    # replace any '\n' (newlines) with ‰
+    STRING_ARRAY=${TEXT_STRING//'\n'/$'‰'}
+
+    # split by ‰ and make an array of each line
+    IFS=$'‰' read -r -a ARRAY_OF_LINES <<< "$STRING_ARRAY"
+
+    # Find the longest line in array
+    # by iterating through it and checking if its longer
+    # than the previous longest line.
+    BOX_LONGEST_LINE=0
+    BOX_EMOJI_PADDING=0
+    
+    for CURRENT_LINE in "${ARRAY_OF_LINES[@]}"; do
+
+        # Count line length
+        LENGTH_OF_CURRENT_LINE=${#CURRENT_LINE}
+
+        # If there is a non-ascii character, Increase the line by + 1
+        # And set BOX_EMOJI_PADDING to +1
+        if [[ $CURRENT_LINE = *[![:ascii:]]* ]]; then
+            BOX_EMOJI_PADDING=1
+            LENGTH_OF_CURRENT_LINE=$(( $LENGTH_OF_CURRENT_LINE + $BOX_EMOJI_PADDING ))
+        fi
+
+        # echo "length: $LENGTH_OF_CURRENT_LINE index: $CURRENT_LINE"
+
+        # Check for NON-ASCII characters and increase
+        # remove padding -1 for each because emoji are
+        # 2-character width icons.
+        # if [[ $CURRENT_LINE = *[![:ascii:]]* ]]; then
+        #     ZZZZ_ONLY="${CURRENT_LINE//[^[:ascii:]]/ZZZZZ}"
+        #     INSTANCES=${#ZZZZ_ONLY}
+            
+        #     LINE_EMOJI_COUNT=${ZZZZ_ONLY//ZZZZZ/}
+        #     LINE_EMOJI_COUNT=${#LINE_EMOJI_COUNT}
+        #     LINE_EMOJI_COUNT=$(( ( $INSTANCES - $LINE_EMOJI_COUNT ) / 5 ))
+
+        #     # LINE_EMOJI_PADDING=$LINE_EMOJI_COUNT
+        #     # printf "LINE_EMOJI_COUNT=$BOX_EMOJI_COUNT \n"
+        #     # printf "LINE_EMOJI_PADDING=$LINE_EMOJI_PADDING \n"
+
+        #     LENGTH_OF_CURRENT_LINE=$(( $LENGTH_OF_CURRENT_LINE + $LINE_EMOJI_COUNT ))
+        # fi
+
+        # If the length of line is biggest, set it.
+        if [ $LENGTH_OF_CURRENT_LINE -gt $BOX_LONGEST_LINE ]; then
+            BOX_LONGEST_LINE=$LENGTH_OF_CURRENT_LINE
+        fi
+
+    done
+
+}
+
+
+
 # Create a box with various qualities.
 # - Font colour
 # - Background colour
@@ -16,185 +78,144 @@ repeat_character(){
 box()
 {   
 
+    # Inputs into the script
     CLASSES=$1
     TEXT_STRING=$2
 
-    PADDING_PX=0
-    PADDING_PY=0
+    # This is used to identify this item and
+    # let the parser create global variables
+    # with this prefix.
+    PREFIX="BOX"
 
-    PADDING_PT=0
-    PADDING_PR=0
-    PADDING_PB=0
-    PADDING_PL=0
+    # Default Padding Sizes
+    declare "${PREFIX}_PX"=0
+    declare "${PREFIX}_PY"=0
+
+    declare "${PREFIX}_PT"=0
+    declare "${PREFIX}_PR"=0
+    declare "${PREFIX}_PB"=0
+    declare "${PREFIX}_PL"=0
     
-    # Define the icons to use to draw the box
-    EDGE_TL="${ICON_TL}"
-    EDGE_T="${ICON_FH}"
-    EDGE_TR="${ICON_TR}"
-    EDGE_R="${ICON_FV}"
-    EDGE_BR="${ICON_BR}"
-    EDGE_B="${ICON_FH}"
-    EDGE_BL="${ICON_BL}"
-    EDGE_L="${ICON_FV}"
+    # Default edge icons
+    declare "${PREFIX}_EDGE_TL"="${ICON_TL}"
+    declare "${PREFIX}_EDGE_T"="${ICON_FH}"
+    declare "${PREFIX}_EDGE_TR"="${ICON_TR}"
+    declare "${PREFIX}_EDGE_R"="${ICON_FV}"
+    declare "${PREFIX}_EDGE_BR"="${ICON_BR}"
+    declare "${PREFIX}_EDGE_B"="${ICON_FH}"
+    declare "${PREFIX}_EDGE_BL"="${ICON_BL}"
+    declare "${PREFIX}_EDGE_L"="${ICON_FV}"
 
 
-    # Create an array of text lines 
-    TEXT_STRING=${TEXT_STRING//'\n'/$'yyyy'}
-    IFS=$'yyyy' read -r -a ARRAY_OF_LINES <<< "$TEXT_STRING"
+    # Figure out the longest line width.
+    longest_line_width "$TEXT_STRING"
 
-    # Find the longest line in array
-    LONGEST_LINE=0
-    LENGTH_OF_CURRENT_LINE=0
-    ICON_PADDING=0
-    for CURRENT_LINE in "${ARRAY_OF_LINES[@]}"; do
-
-        LENGTH_OF_CURRENT_LINE=${#CURRENT_LINE}
-
-        if [[ $CURRENT_LINE = *[![:ascii:]]* ]]; then
-            ICON_PADDING=1
-            LENGTH_OF_CURRENT_LINE=$(( $LENGTH_OF_CURRENT_LINE + $ICON_PADDING ))
-        fi
-
-        # If the length of line is biggest, set.
-        if [ $LENGTH_OF_CURRENT_LINE -gt $LONGEST_LINE ]; then
-            LONGEST_LINE=$LENGTH_OF_CURRENT_LINE
-        fi
-
-    done
+    # Source and run the parse_classes script.
+    # This will override any defaults by setting them
+    # to specified values
+    . ../utils/parse_classes.sh "${PREFIX}" "${CLASSES}"
 
 
-
-    # class list to be parsed.
-    # loop through each class name and create a CLASS variable
-    # set inter-field separator (IFS) to a space and loop
-    export IFS=" "
-    for CLASS in $CLASSES; do
-
-        # Split by underscores and assign each part to a variable
-        IFS="_" read PARAMETER PRIMARY SECONDARY <<< "$CLASS"
-        # printf "$PARAMETER : $COLOUR : $SATURATION\n"
-
-        # create new variable TEXT_COLOUR, BG_COLOUR, BORDER_COLOUR, etc...
-        # And set it to the full string.
-        if [[ $PARAMETER == 'TEXT' ]] || [[ $PARAMETER == 'BG' ]] || [[ $PARAMETER == 'BORDER' ]]; then
-            declare COLOUR_$PARAMETER=${!CLASS}
-        fi
-
-        if [[ $PARAMETER == PX* ]] || [[ $PARAMETER == PY* ]] || [[ $PARAMETER == PT* ]] || [[ $PARAMETER == PR* ]] || [[ $PARAMETER == PB* ]] || [[ $PARAMETER == PL* ]]; then
-            declare PADDING_$PARAMETER=${!CLASS}
-        fi 
-        
-        if [[ $PARAMETER == EDGE* ]] && [[ $SECONDARY == "" ]] ; then
-            varname="${PARAMETER}_${PRIMARY}_TL"; declare EDGE_TL=${!varname}
-            varname="${PARAMETER}_${PRIMARY}_T";  declare EDGE_T=${!varname}
-            varname="${PARAMETER}_${PRIMARY}_TR"; declare EDGE_TR=${!varname}
-            varname="${PARAMETER}_${PRIMARY}_R";  declare EDGE_R=${!varname}
-            varname="${PARAMETER}_${PRIMARY}_BR"; declare EDGE_BR=${!varname}
-            varname="${PARAMETER}_${PRIMARY}_B";  declare EDGE_B=${!varname}
-            varname="${PARAMETER}_${PRIMARY}_BL"; declare EDGE_BL=${!varname}
-            varname="${PARAMETER}_${PRIMARY}_L";  declare EDGE_L=${!varname}
-        fi 
-
-        if [[ $PARAMETER == EDGE* ]] && ! [[ $SECONDARY == "" ]] ; then
-            varname="${CLASS}"
-            declare EDGE_${SECONDARY}=${!varname}
-        fi
-
-        # VARIABLENAME=COLOUR_$PARAMETER && printf "${!VARIABLENAME} $VARIABLENAME\n"
-    done
-
-
-    # HORIZONTAL PADDING
-
-    # Generate the horizontal padding lines
-    if ! [ $PADDING_PX -eq "0" ]; then
-        PX_SPACING=$(repeat_character $PADDING_PX " ")
-    fi
-
-    # Generate the TOP padding lines if its set
-    if ! [ $PADDING_PR -eq "0" ]; then
-        PR_SPACING=$(repeat_character $PADDING_PR " ")
-    fi
-
-    # Generate the BOTTOM padding lines if its set
-    if ! [ $PADDING_PL -eq "0" ]; then
-        PL_SPACING=$(repeat_character $PADDING_PL " ")
-    fi
-
-
-    # Figure out lengths and add the padding x2 (on each side)
-    STRING_LENGTH=${#TEXT_STRING}
-    WIDTH=$(( $PADDING_PL + $PADDING_PX + ${LONGEST_LINE} + $PADDING_PX + $PADDING_PR ))
-
+    # Figure out the width of the box WITHOUT EDGES
+    # Take the longest line (figured out above)
+    # Add any padding from "PX_", "PR_" or "PL_"
+    BOX_WIDTH=$(( $BOX_PL + $BOX_PX + ${BOX_LONGEST_LINE} + $BOX_PX + $BOX_PR ))
 
     # Create the horizontal top and bottom bar
-    EDGE_T=$(repeat_character ${WIDTH} ${EDGE_T})
-    EDGE_B=$(repeat_character ${WIDTH} ${EDGE_B})
+    # ───────────────────── ─────────────────────
+    BOX_EDGE_T=$(repeat_character ${BOX_WIDTH} ${BOX_EDGE_T})
+    BOX_EDGE_B=$(repeat_character ${BOX_WIDTH} ${BOX_EDGE_B})
 
     # Create the horizontal padding
-    PR_PADDING=$(repeat_character $PADDING_PR " ")
-    PL_PADDING=$(repeat_character $PADDING_PL " ")
-    PX_PADDING=$(repeat_character $PADDING_PX " ")
+    # ░░▒▒TEXT▒▒░░░░
+    BOX_PR=$(repeat_character $BOX_PR " ")
+    BOX_PL=$(repeat_character $BOX_PL " ")
+    BOX_PX=$(repeat_character $BOX_PX " ")
 
     # Create an empty space for vertical padding
-    VERTICAL_PADDING=$(repeat_character ${WIDTH} " ")
+    # ░░░░░░░░░░░░░░░░
+    BOX_VERTICAL=$(repeat_character ${BOX_WIDTH} " ")
 
-    # Create the vertical spacer padding line
-    VERTICAL_PADDING_LINE="${COLOUR_BG}${EDGE_L}${VERTICAL_PADDING}${EDGE_R}${RESET_BG}\n"
+    # Create the vertical spacer padding line with edges
+    # │░░░░░░░░░░░░░░░░│
+    BOX_VERTICAL_LINE="${BOX_BG_COLOUR}${BOX_EDGE_L}${BOX_VERTICAL}${BOX_EDGE_R}${RESET_BG} \n"
     
 
 
     # VERTICAL PADDING
-    
     # Generate the vertical padding lines
-    if ! [ $PADDING_PY -eq "0" ]; then
-        PY_LINE=$(repeat_character $PADDING_PY "${VERTICAL_PADDING_LINE}")
+    # │░░░░░░░░░░░░░░░░│
+    # │░░░░░░░░░░░░░░░░│
+    if ! [ $BOX_PY -eq "0" ]; then
+        BOX_PY_LINE=$(repeat_character $BOX_PY "${BOX_VERTICAL_LINE}")
     fi
 
     # Generate the TOP padding lines if its set
-    if ! [ $PADDING_PT -eq "0" ]; then
-        PT_LINE=$(repeat_character $PADDING_PT "${VERTICAL_PADDING_LINE}")
+    # │░░░░░░░░░░░░░░░░│
+    # │░░░░░░░░░░░░░░░░│
+    # │░░░░░░░░░░░░░░░░│
+    if ! [ $BOX_PT -eq "0" ]; then
+        BOX_PT_LINE=$(repeat_character $BOX_PT "${BOX_VERTICAL_LINE}")
     fi
 
     # Generate the BOTTOM padding lines if its set
-    if ! [ $PADDING_PB -eq "0" ]; then
-        PB_LINE=$(repeat_character $PADDING_PB "${VERTICAL_PADDING_LINE}")
+    # │░░░░░░░░░░░░░░░░│
+    # │░░░░░░░░░░░░░░░░│
+    # │░░░░░░░░░░░░░░░░│
+    if ! [ $BOX_PB -eq "0" ]; then
+        BOX_PB_LINE=$(repeat_character $BOX_PB "${BOX_VERTICAL_LINE}")
     fi
 
 
-
-
-
-    # Generate a text-line for each line in array
-    TEXT_LINE=""
+    # Start empty
+    BOX_TEXT_LINE=""
 
     # Loop through Array to add extra padding
+    # Generate a text-line for each line in array
+    # ARRAY_OF_LINES[1] = "textline 1 🚀"
+    # ARRAY_OF_LINES[2] = "text 2"
+    # ARRAY_OF_LINES[3] = "text line three 🏠"
     for LINE in "${ARRAY_OF_LINES[@]}"; do
 
+        # skip empty.
         if [[ -z $LINE ]]; then continue; fi
 
-        # Extra line padding
-        LINE_LENGTH=${#LINE}
-        EXTRA_PR_LENGTH=$(( $LONGEST_LINE - $LINE_LENGTH - $ICON_PADDING ))
-        EXTRA_PR=$(repeat_character $EXTRA_PR_LENGTH " ")
+        # Current Line length
+        # i.e. 20
+        BOX_CURRENT_LINE_LENGTH=${#LINE}
 
+        # If line contains emoji +1 for double-character
+        # "textline 1 🚀" 
+        if [[ $LINE = *[![:ascii:]]* ]]; then
+            BOX_CURRENT_LINE_LENGTH=$(( $BOX_CURRENT_LINE_LENGTH + 1 ))
+        fi
 
-        # printf "$LINE\n"
-        TEXT_LINE="${TEXT_LINE}${COLOUR_BG}"
-        TEXT_LINE="${TEXT_LINE}${EDGE_L}"
-        TEXT_LINE="${TEXT_LINE}${PX_PADDING}"
-        TEXT_LINE="${TEXT_LINE}${PL_PADDING}"
-            TEXT_LINE="${TEXT_LINE}${RESET_TEXT}"
-            TEXT_LINE="${TEXT_LINE}${COLOUR_TEXT}"
-            TEXT_LINE="${TEXT_LINE}${LINE}"
-            TEXT_LINE="${TEXT_LINE}${EXTRA_PR}"
-            TEXT_LINE="${TEXT_LINE}${RESET_TEXT}"
-        TEXT_LINE="${TEXT_LINE}${COLOUR_BORDER}"
-        TEXT_LINE="${TEXT_LINE}${PR_PADDING}"
-        TEXT_LINE="${TEXT_LINE}${PX_PADDING}"
-        TEXT_LINE="${TEXT_LINE}${EDGE_R}"
-        TEXT_LINE="${TEXT_LINE}${RESET_BG}"
-        TEXT_LINE="${TEXT_LINE}\n"
+        # Set the length of the right padding
+        # taking into consideration any emoji or not
+        # current longest line - minus current line text = space left
+        # 
+        BOX_EXTRA_PR_LENGTH=$(( $BOX_LONGEST_LINE - $BOX_CURRENT_LINE_LENGTH ))
+
+        # ░
+        BOX_EXTRA_PR=$(repeat_character $BOX_EXTRA_PR_LENGTH " ")
+
+        # Create text line
+        # │░textbox here 123 🚀░░│
+        BOX_TEXT_LINE="${BOX_TEXT_LINE}${BOX_BG_COLOUR}"
+        BOX_TEXT_LINE="${BOX_TEXT_LINE}${BOX_EDGE_L}"
+        BOX_TEXT_LINE="${BOX_TEXT_LINE}${BOX_PX}"
+        BOX_TEXT_LINE="${BOX_TEXT_LINE}${BOX_PL}"
+            BOX_TEXT_LINE="${BOX_TEXT_LINE}${RESET_TEXT}"
+            BOX_TEXT_LINE="${BOX_TEXT_LINE}${BOX_TEXT_COLOUR}"
+            BOX_TEXT_LINE="${BOX_TEXT_LINE}${LINE}"
+            BOX_TEXT_LINE="${BOX_TEXT_LINE}${BOX_EXTRA_PR}"
+            BOX_TEXT_LINE="${BOX_TEXT_LINE}${RESET_TEXT}"
+        BOX_TEXT_LINE="${BOX_TEXT_LINE}${BOX_BORDER_COLOUR}"
+        BOX_TEXT_LINE="${BOX_TEXT_LINE}${BOX_PR}"
+        BOX_TEXT_LINE="${BOX_TEXT_LINE}${BOX_PX}"
+        BOX_TEXT_LINE="${BOX_TEXT_LINE}${BOX_EDGE_R}"
+        BOX_TEXT_LINE="${BOX_TEXT_LINE}${RESET_BG}"
+        BOX_TEXT_LINE="${BOX_TEXT_LINE}\n"
         
     done
 
@@ -203,31 +224,31 @@ box()
     BOX=""
 
     # Set background colour
-    BOX="${BOX}${COLOUR_BG}"
+    BOX="${BOX}${BOX_BG_COLOUR}"
 
     # Top line
-    BOX="${BOX}${COLOUR_BORDER}"
-    BOX="${BOX}${EDGE_TL}"
-    BOX="${BOX}${EDGE_T}"
-    BOX="${BOX}${EDGE_TR}"
+    BOX="${BOX}${BOX_BORDER_COLOUR}"
+    BOX="${BOX}${BOX_EDGE_TL}"
+    BOX="${BOX}${BOX_EDGE_T}"
+    BOX="${BOX}${BOX_EDGE_TR}"
     BOX="${BOX}\n"
 
     # Vertical padding lines
-    BOX="${BOX}${PT_LINE}"
-    BOX="${BOX}${PY_LINE}"
+    BOX="${BOX}${BOX_PT_LINE}"
+    BOX="${BOX}${BOX_PY_LINE}"
 
     # Text Line
-    BOX="${BOX}${TEXT_LINE}"
+    BOX="${BOX}${BOX_TEXT_LINE}"
 
     # Vertical padding lines
-    BOX="${BOX}${PY_LINE}"
-    BOX="${BOX}${PB_LINE}"
+    BOX="${BOX}${BOX_PY_LINE}"
+    BOX="${BOX}${BOX_PB_LINE}"
 
     # Bottom line
-    BOX="${BOX}${COLOUR_BG}"
-    BOX="${BOX}${EDGE_BL}"
-    BOX="${BOX}${EDGE_B}"
-    BOX="${BOX}${EDGE_BR}"
+    BOX="${BOX}${BOX_BG_COLOUR}"
+    BOX="${BOX}${BOX_EDGE_BL}"
+    BOX="${BOX}${BOX_EDGE_B}"
+    BOX="${BOX}${BOX_EDGE_BR}"
     BOX="${BOX}${RESET_BG}"
 
     # Reset background colour
