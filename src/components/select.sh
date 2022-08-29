aecee359376f19084cb03fd6275b2c20852d83c7
Andy Pearson <andy@londonparkour.com>
2022-08-29 10:13:35 +0100

    half work selection

--- src/components/select.sh
+++ src/components/select.sh
@@ -17,11 +17,16 @@ function options(){
     # Take first arg as Config file
     CONFIG_FILE=$1
 
+    # Clear the screen
+    clear
+
     # Run file through the parser
     JSON_PARSED=$($PARSER < $CONFIG_FILE)
 
     # Store the length of select array
-    JSON_SELECT_LENGTH=$(getarrlen select)
+    JSON_SELECT_LENGTH=$(getarrlen select.options)
+    JSON_OPTION_HEIGHT=$(getarrlen select.config.option_height)
+    
 
     SELECT_ARRAY=()
     # Loop through select object to get each array
@@ -30,25 +35,63 @@ function options(){
 
         # Create the variables
         declare LOOP_SELECT_INDEX=$LOOP
-        declare LOOP_SELECT_TITLE=$(getkey select[$LOOP].title)
-        declare LOOP_SELECT_DESCRIPTION=$(getkey select[$LOOP].description)
-        declare LOOP_SELECT_STYLE=$(getkey select[$LOOP].style)
-        declare LOOP_SELECT_COMMAND=$(getkey select[$LOOP].command)
-        LOOP_SELECT_COMMAND=${LOOP_SELECT_COMMAND//\\/}  # Remove the forward escape slashes in the command field
+        declare LOOP_SELECT_TITLE=$(getkey select.options[$LOOP].title)
+        declare LOOP_SELECT_TITLE_STYLE=$(getkey select.options[$LOOP].title_style)
+        declare LOOP_SELECT_DESCRIPTION=$(getkey select.options[$LOOP].description)
+        declare LOOP_SELECT_DESCRIPTION_STYLE=$(getkey select.options[$LOOP].description_style)
+        declare LOOP_SELECT_COMMAND=$(getkey select.options[$LOOP].command)
+        declare LOOP_SELECT_HEIGHT=$(getkey select.options[$LOOP].height)
+        declare LOOP_SELECT_SIDEBAR_STYLE=$(getkey select.options[$LOOP].sidebar_style)
 
-        # Create the menu option
+# echo "LOOP_SELECT_SIDEBAR_STYLE: $LOOP_SELECT_SIDEBAR_STYLE"
+
+        # Remove the forward escape slashes in the command field
+        LOOP_SELECT_COMMAND=${LOOP_SELECT_COMMAND//\\/}  
+
+
+        # DEFAULT STYLES
+        TITLE_STYLE_TEXT_COLOUR="${TEXT_GRAY_100}"
+
+        DESCRIPTION_STYLE_TEXT_COLOUR="${TEXT_GRAY_400}"
+
+        SIDELINE_STYLE_BORDER_COLOUR="${BORDER_GRAY_900}"
+
+        # OVERRIDES
+        #
+        # Source and run the parse_classes script.
+        # This will override any default variables
+        # by setting them to supplied user values.
+        #
+        # @return $PREFIX_VARIABLE
+        source $SPINNAKER_TOOLS_FOLDER/utils/parse_classes.sh "TITLE_STYLE" "${LOOP_SELECT_TITLE_STYLE}"
+        source $SPINNAKER_TOOLS_FOLDER/utils/parse_classes.sh "DESCRIPTION_STYLE" "${LOOP_SELECT_DESCRIPTION_STYLE}"
 
         # Create a box
-        BOX=$(./box.sh "${LOOP_SELECT_STYLE}"  "${ICON_ROCKET}${LOOP_SELECT_TITLE}\n$LOOP_SELECT_DESCRIPTION")
-        VALUE=$( printf "$BOX" )
-        SELECT_ARRAY+=("${VALUE}")
+        OPTION_TEXT=''
+        OPTION_TEXT+="${SIDELINE_STYLE_BORDER_COLOUR}"
+        OPTION_TEXT+="${ICON_SIDE_L} "
+        OPTION_TEXT+="${TITLE_STYLE_TEXT_COLOUR}"
+        OPTION_TEXT+="${LOOP_SELECT_TITLE}\n"
+        OPTION_TEXT+="${SIDELINE_STYLE_BORDER_COLOUR}"
+        OPTION_TEXT+="${ICON_SIDE_L} "
+        OPTION_TEXT+="${DESCRIPTION_STYLE_TEXT_COLOUR}"
+        OPTION_TEXT+="${LOOP_SELECT_DESCRIPTION}"
+
+        # Output value
+        OPTION_VALUE=$( printf "$OPTION_TEXT" )
+
+        # Add to array
+        SELECT_ARRAY+=("${OPTION_VALUE}")
 
         # Create environment variables of results
         declare SELECT_ARRAY_INDEX_$LOOP="${LOOP_SELECT_INDEX}"
         declare SELECT_ARRAY_TITLE_$LOOP="${LOOP_SELECT_TITLE}"
+        declare SELECT_ARRAY_TITLE_STYLE_$LOOP="${LOOP_SELECT_TITLE_STYLE}"
         declare SELECT_ARRAY_DESCRIPTION_$LOOP="${LOOP_SELECT_DESCRIPTION}"
-        declare SELECT_ARRAY_STYLE_$LOOP="${LOOP_SELECT_STYLE}"
+        declare SELECT_ARRAY_DESCRIPTION_STYLE_$LOOP="${LOOP_SELECT_DESCRIPTION_STYLE}"
         declare SELECT_ARRAY_COMMAND_$LOOP="${LOOP_SELECT_COMMAND}"
+        declare SELECT_ARRAY_HEIGHT_$LOOP="${LOOP_SELECT_HEIGHT}"
+        declare SELECT_ARRAY_SIDEBAR_STYLE_$LOOP="${LOOP_SELECT_SIDEBAR_STYLE}"
 
     done
 
