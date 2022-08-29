8e2562479a7816ef1043b4c13bbda56df57abd32
Andy Pearson <andy@londonparkour.com>
2022-08-29 14:53:45 +0100

    Simple Alerts

--- src/components/alert.sh
+++ src/components/alert.sh
@@ -5,7 +5,27 @@ BOX=$SPINNAKER_COMPONENTS_FOLDER/box.sh
 alert()
 {
     TEXT=$1
-    INFO=$2
-    $BOX "BORDER_SKY_500 W_FULL PY_1 TEXT_SKY_400 ALIGN_CENTER" "Info alert!\nmulti\n${ICON_ROCKET} ROCKET"
+    TYPE=$2
+
+    if [[ "$TYPE" == "INFO" ]]; then
+        $BOX "BORDER_SKY_500 TEXT_SKY_400 W_FULL PY_1 ALIGN_CENTER" "${ICON_ALERT}\n$TEXT"
+    fi
+
+    if [[ "$TYPE" == "DANGER" ]]; then
+        $BOX "BORDER_RED_500 TEXT_RED_400 W_FULL PY_1 ALIGN_CENTER" "${ICON_STOP}\n$TEXT"
+    fi
+
+    if [[ "$TYPE" == "SUCCESS" ]]; then
+        $BOX "BORDER_EMERALD_500 TEXT_EMERALD_400 W_FULL PY_1 ALIGN_CENTER" "${ICON_TICK}\n$TEXT"
+    fi
+
+    if [[ "$TYPE" == "WARN" ]]; then
+        $BOX "BORDER_AMBER_500 TEXT_AMBER_400 W_FULL PY_1 ALIGN_CENTER" "${ICON_LIMIT}\n$TEXT"
+    fi
+
+    if [[ "$TYPE" == "DARK" ]]; then
+        $BOX "BORDER_GRAY_600 TEXT_GRAY_600 W_FULL PY_1 ALIGN_CENTER" "${ICON_CUBE}\n$TEXT"
+    fi
 }
+
 alert "$@"
